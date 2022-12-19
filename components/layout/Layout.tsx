9f77ca7a15036998aa0d75c6bfd908e6e8e7b2ab
Dammy Ola <dammyolapaix@gmail.com>
2022-12-19 14:59:59 +0000

    Remove MUI and its related dependencies

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,22 +1,10 @@
-import React, { useState, ReactNode } from 'react'
+import React, { ReactNode } from 'react'
 import Head from 'next/head'
-import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
-import CssBaseline from '@mui/material/CssBaseline'
-import Drawer from '@mui/material/Drawer'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import DrawerLinks from './DrawerLinks'
-
-const drawerWidth = 240
 
 type Props = {
   title: string
   description: string
   keywords: string
-  window?: () => Window
   children: ReactNode
 }
 
@@ -24,18 +12,8 @@ export default function Layout({
   title,
   description,
   keywords,
-  window,
   children,
 }: Props) {
-  const [mobileOpen, setMobileOpen] = useState<boolean>(false)
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen)
-  }
-
-  const container =
-    window !== undefined ? () => window().document.body : undefined
-
   return (
     <>
       <Head>
@@ -47,82 +25,7 @@ export default function Layout({
         <meta name="keywords" content={keywords} />
       </Head>
 
-      <main>
-        <Box sx={{ display: 'flex' }}>
-          <CssBaseline />
-          <AppBar
-            position="fixed"
-            sx={{
-              width: { sm: `calc(100% - ${drawerWidth}px)` },
-              ml: { sm: `${drawerWidth}px` },
-            }}
-          >
-            <Toolbar>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                edge="start"
-                onClick={handleDrawerToggle}
-                sx={{ mr: 2, display: { sm: 'none' } }}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" noWrap component="div">
-                Jesus Never Fails Ministry
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <Box
-            component="nav"
-            sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-            aria-label="mailbox folders"
-          >
-            {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-            <Drawer
-              container={container}
-              variant="temporary"
-              open={mobileOpen}
-              onClose={handleDrawerToggle}
-              ModalProps={{
-                keepMounted: true, // Better open performance on mobile.
-              }}
-              sx={{
-                display: { xs: 'block', sm: 'none' },
-                '& .MuiDrawer-paper': {
-                  boxSizing: 'border-box',
-                  width: drawerWidth,
-                },
-              }}
-            >
-              <DrawerLinks />
-            </Drawer>
-            <Drawer
-              variant="permanent"
-              sx={{
-                display: { xs: 'none', sm: 'block' },
-                '& .MuiDrawer-paper': {
-                  boxSizing: 'border-box',
-                  width: drawerWidth,
-                },
-              }}
-              open
-            >
-              <DrawerLinks />
-            </Drawer>
-          </Box>
-          <Box
-            component="main"
-            sx={{
-              flexGrow: 1,
-              p: 3,
-              width: { sm: `calc(100% - ${drawerWidth}px)` },
-            }}
-          >
-            <Toolbar />
-            {children}
-          </Box>
-        </Box>
-      </main>
+      <main>{children}</main>
     </>
   )
 }
