3ee29d72cf5695a31b132a90d6040632f0c4d71d
Dammy Ola <dammyolapaix@gmail.com>
2022-12-24 14:28:25 +0000

    Display members present for a church service

--- features/attendance/components/Attendances.tsx
+++ features/attendance/components/Attendances.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import React, { FC } from 'react'
 import { MdAdd } from 'react-icons/md'
+import { MemberItem } from '../../member'
 import { IAttendancesRes } from '../index'
 
 const Attendances: FC<{ attendancesRes: IAttendancesRes }> = ({
@@ -20,6 +21,24 @@ const Attendances: FC<{ attendancesRes: IAttendancesRes }> = ({
             <MdAdd />
             <div className="">New</div>
           </Link>
+        </div>
+
+        <div className="overflow-x-auto">
+          <table className="w-full">
+            <thead className="text-secondary">
+              <tr className="text-left border">
+                <th className="p-3">Name</th>
+                <th>Sex</th>
+                <th>Age</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {attendancesRes.attendances.map((attendance) => (
+                <MemberItem key={attendance._id} member={attendance.member} />
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </section>
