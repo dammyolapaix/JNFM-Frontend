9323f0b1efdc91b1cef62433f3cf4054b6879120
Dammy Ola <dammyolapaix@gmail.com>
2022-12-29 10:46:28 +0000

    Added NoRecordFound component

--- components/index.ts
+++ components/index.ts
@@ -1,10 +1,19 @@
-
 import CustomButton from './form/CustomButton'
 import CustomDropDown from './form/CustomDropDown'
 import CustomTextField from './form/CustomTextField'
 import Layout from './layout/Layout'
 import SideNav from './layout/SideNav'
+import NoRecordFound from './NoRecordFound'
 import QueryResult from './QueryResult'
 import Spinner from './Spinner'
 
-export { CustomButton, CustomDropDown, CustomTextField, Layout, SideNav, QueryResult, Spinner }
+export {
+  CustomButton,
+  CustomDropDown,
+  CustomTextField,
+  Layout,
+  SideNav,
+  QueryResult,
+  Spinner,
+  NoRecordFound,
+}
