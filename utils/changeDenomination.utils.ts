4030e1aa00b6fc372bbde9f38370dd4c6cb1faa8
Dammy Ola <dammyolapaix@gmail.com>
2023-01-11 17:19:43 +0000

    Modify the changeToHigerDenomination function to display in a currency format

--- utils/changeDenomination.utils.ts
+++ utils/changeDenomination.utils.ts
@@ -1,2 +1,7 @@
 export const changeToHigherDenomination = (amount: number): string =>
-  (amount / 100).toFixed(2)
+  new Intl.NumberFormat(undefined, {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+    style: 'currency',
+    currency: 'GHS',
+  }).format(amount / 100)
