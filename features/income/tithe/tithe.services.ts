7970c38a93a212f221dad5cdbac2884895e4beb1
Dammy Ola <dammyolapaix@gmail.com>
2023-01-12 21:14:07 +0000

    Added the logic to add a new tithe

--- features/income/tithe/tithe.services.ts
+++ features/income/tithe/tithe.services.ts
@@ -1,7 +1,12 @@
 import { makeRequest } from '../../../lib'
-import { ITithesRes } from './tithe.interfaces'
+import { IBaseTithe, ITitheRes, ITithesRes } from './index'
 
 export const getTithes = async () => {
   const { data } = await makeRequest.get<ITithesRes>('/tithes')
+  return data
+}
+
+export const addTithe = async (tithe: IBaseTithe) => {
+  const { data } = await makeRequest.post<ITitheRes>('/tithes', tithe)
   return data
 }
