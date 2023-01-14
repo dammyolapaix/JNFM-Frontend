368c79da16e8afeb8fd0b0924a8d6d2668a50d77
Dammy Ola <dammyolapaix@gmail.com>
2023-01-14 18:35:36 +0000

    Display the balance and the total income and expenditure for the cashbook

--- features/cashBook/components/CashBooks.tsx
+++ features/cashBook/components/CashBooks.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { FC } from 'react'
+import { GiMoneyStack, GiPayMoney, GiTwoCoins } from 'react-icons/gi'
 import { MdAdd } from 'react-icons/md'
 import { NoRecordFound } from '../../../components'
 import { changeToHigherDenomination } from '../../../utils'
@@ -7,7 +8,7 @@ import { CashBookItem, ICashBooksRes } from '../index'
 
 const CashBooks: FC<{
   cashBooksRes: ICashBooksRes
-}> = ({ cashBooksRes: { cashBooks, count } }) => {
+}> = ({ cashBooksRes: { cashBooks, count, totalCashBook } }) => {
   return (
     <section>
       {count === 0 ? (
@@ -18,10 +19,69 @@ const CashBooks: FC<{
         />
       ) : (
         <>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mb-10">
+            <Link
+              href={`/incomes/tithes`}
+              className="bg-green-600 text-white hover:bg-green-500 rounded-md flex flex-col items-center justify-center p-5 font-semibold"
+            >
+              <GiMoneyStack className="text-5xl" />
+              <div className="my-3">Income</div>
+              <div className="text-xl font-bold">
+                {changeToHigherDenomination(
+                  totalCashBook &&
+                    totalCashBook.length > 0 &&
+                    totalCashBook[0].totalIncome.length > 0
+                    ? totalCashBook[0].totalIncome[0].totalIncome
+                    : 0
+                )}
+              </div>
+            </Link>
+            <Link
+              href={`/incomes/welfares`}
+              className="bg-red-600 text-white hover:bg-red-500 rounded-md flex flex-col items-center justify-center p-5 font-semibold"
+            >
+              <GiPayMoney className="text-5xl" />
+              <div className="my-3">Expenditures</div>
+              <div className="text-xl font-bold">
+                {changeToHigherDenomination(
+                  totalCashBook &&
+                    totalCashBook.length > 0 &&
+                    totalCashBook[0].totalExpenditure &&
+                    totalCashBook[0].totalExpenditure.length > 0
+                    ? totalCashBook[0].totalExpenditure[0].totalExpenditure
+                    : 0
+                )}
+              </div>
+            </Link>
+            <Link
+              href={`/incomes/tithes`}
+              className="bg-secondary text-white hover:bg-tertiary rounded-md flex flex-col items-center justify-center p-5 font-semibold"
+            >
+              <GiTwoCoins className="text-5xl" />
+              <div className="my-3">Balance</div>
+              <div className="text-xl font-bold">
+                {changeToHigherDenomination(
+                  totalCashBook &&
+                    totalCashBook.length > 0 &&
+                    totalCashBook[0].balance.length > 0
+                    ? totalCashBook[0].balance[0].balance
+                    : 0
+                )}
+              </div>
+            </Link>
+          </div>
           <div className="shadow-md">
             <div className="flex justify-between items-center">
               <h1 className="font-extrabold text-2xl mb-5 text-secondary">
-                Cashbook (Ghc {changeToHigherDenomination(37789)})
+                Cashbook (
+                {changeToHigherDenomination(
+                  totalCashBook &&
+                    totalCashBook.length > 0 &&
+                    totalCashBook[0].balance.length > 0
+                    ? totalCashBook[0].balance[0].balance
+                    : 0
+                )}
+                )
               </h1>
               <Link
                 href={`#`}
