5442d78b9e777761f241eb96895b334ca8f8343d
Dammy Ola <dammyolapaix@gmail.com>
2023-02-05 19:34:58 +0000

    Shifted attendance to the cell

--- features/member/components/MemberItem.tsx
+++ features/member/components/MemberItem.tsx
@@ -6,9 +6,17 @@ import { MdEdit, MdRemoveRedEye } from 'react-icons/md'
 import { getAge } from '../../../utils'
 
 const MemberItem: FC<{ member: IMember }> = ({
-  member: { _id, fullName, gender, dateOfBirth, cell },
+  member: { _id, fullName, gender, dateOfBirth, cell, attendances },
 }) => {
-  const { route } = useRouter()
+  const { route, query } = useRouter()
+
+  const memberIsPresent =
+    attendances &&
+    attendances.some(
+      (attendance) =>
+        typeof attendance.churchService === 'string' &&
+        attendance.churchService === query.serviceId
+    )
 
   return (
     <>
@@ -16,27 +24,37 @@ const MemberItem: FC<{ member: IMember }> = ({
         <td className="p-3">{fullName ? fullName : ''}</td>
         <td>{gender ? gender?.at(0) : 'Not Given'}</td>
         <td>{dateOfBirth ? getAge(dateOfBirth) : 'Not Given'}</td>
-        {route !== '/cells/[id]' && (
+        {!route.includes('/cells/[id]') && (
           <td>
             {cell && cell?.cell && typeof cell?.cell === 'object'
               ? cell?.cell?.name
               : 'Not Given'}
           </td>
         )}
-        <td className="flex items-center mt-2">
-          <Link
-            href={`/members/${_id}`}
-            className="bg-secondary hover:bg-tertiary text-white rounded-md py-2 px-4 mr-3"
-          >
-            <MdRemoveRedEye />
-          </Link>
-          <Link
-            href={`/members/${_id}/edit`}
-            className="bg-primary hover:bg-tertiary text-white rounded-md py-2 px-4"
-          >
-            <MdEdit />
-          </Link>
-        </td>
+        {route === '/cells/[id]/[serviceId]/attendance' ? (
+          <td>
+            {typeof memberIsPresent !== 'undefined' && memberIsPresent ? (
+              <span className="text-green-500">Present</span>
+            ) : (
+              <span className="text-red-500">Absent</span>
+            )}
+          </td>
+        ) : (
+          <td className="flex items-center mt-2">
+            <Link
+              href={`/members/${_id}`}
+              className="bg-secondary hover:bg-tertiary text-white rounded-md py-2 px-4 mr-3"
+            >
+              <MdRemoveRedEye />
+            </Link>
+            <Link
+              href={`/members/${_id}/edit`}
+              className="bg-primary hover:bg-tertiary text-white rounded-md py-2 px-4"
+            >
+              <MdEdit />
+            </Link>
+          </td>
+        )}
       </tr>
     </>
   )
--- features/member/components/MembersTable.tsx
+++ features/member/components/MembersTable.tsx
@@ -15,7 +15,7 @@ const MembersTable: FC<{
             <th className="p-3">Name</th>
             <th>Sex</th>
             <th>Age</th>
-            {route !== '/cells/[id]' && <th>Cell</th>}
+            {!route.includes('/cells/[id]') && <th>Cell</th>}
             <th>Action</th>
           </tr>
         </thead>
