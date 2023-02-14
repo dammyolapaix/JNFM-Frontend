1208f5ccb761572f1ecf54f340d1b91b309c476c
Dammy Ola <dammyolapaix@gmail.com>
2023-02-14 14:52:43 +0000

    Added credentials and cookie to department services/endpoints

--- features/department/department.services.ts
+++ features/department/department.services.ts
@@ -8,55 +8,102 @@ import {
   IDepartmentsRes,
 } from './index'
 
-export const getDepartments = async () => {
-  const { data } = await makeRequest.get<IDepartmentsRes>('/departments')
+export const getDepartments = async (cookie?: string) => {
+  const { data } = await makeRequest.get<IDepartmentsRes>('/departments', {
+    withCredentials: true,
+    headers: {
+      Cookie: cookie,
+    },
+  })
   return data
 }
 
-export const getSingleDepartmentById = async (id: IDepartment['_id']) => {
-  const { data } = await makeRequest.get<IDepartmentRes>(`/departments/${id}`)
+export const getSingleDepartmentById = async (
+  id: IDepartment['_id'],
+  cookie?: string
+) => {
+  const { data } = await makeRequest.get<IDepartmentRes>(`/departments/${id}`, {
+    withCredentials: true,
+    headers: {
+      Cookie: cookie,
+    },
+  })
   return data
 }
 
-export const addDepartment = async (department: IBaseDepartment) => {
+export const addDepartment = async (
+  department: IBaseDepartment,
+  cookie?: string
+) => {
   const { data } = await makeRequest.post<IDepartmentRes>(
     `/departments`,
-    department
+    department,
+    {
+      withCredentials: true,
+      headers: {
+        Cookie: cookie,
+      },
+    }
   )
+
   return data
 }
 
 export const editDepartment = async (
   id: IDepartment['_id'],
-  department: IBaseDepartment
+  department: IBaseDepartment,
+  cookie?: string
 ) => {
   const { data } = await makeRequest.patch<IDepartmentRes>(
     `/departments/${id}`,
-    department
+    department,
+    {
+      withCredentials: true,
+      headers: {
+        Cookie: cookie,
+      },
+    }
   )
+
   return data
 }
 
 export const addMemberToDepartment = async (
-  departmentMemberReq: IDepartmentMemberReq
+  departmentMemberReq: IDepartmentMemberReq,
+  cookie?: string
 ) => {
   const { departmentId: id, member } = departmentMemberReq
 
   const { data } = await makeRequest.patch<IDepartmentRes>(
     `/departments/${id}/members/add`,
-    { member }
+    member,
+    {
+      withCredentials: true,
+      headers: {
+        Cookie: cookie,
+      },
+    }
   )
+
   return data
 }
 
 export const removeMemberFromDepartment = async (
-  departmentMemberReq: IDepartmentMemberReq
+  departmentMemberReq: IDepartmentMemberReq,
+  cookie?: string
 ) => {
   const { departmentId: id, member } = departmentMemberReq
 
   const { data } = await makeRequest.patch<IDepartmentRes>(
     `/departments/${id}/members/remove`,
-    { member }
+    member,
+    {
+      withCredentials: true,
+      headers: {
+        Cookie: cookie,
+      },
+    }
   )
+
   return data
 }
