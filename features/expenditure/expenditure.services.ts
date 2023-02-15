42909cff9b4759fc9d0e226a47f2ff571fe21193
Dammy Ola <dammyolapaix@gmail.com>
2023-02-15 05:25:23 +0000

    Added credentials and cookie to expenditure and expenditure category services/endpoints, pages, and components

--- features/expenditure/expenditure.services.ts
+++ features/expenditure/expenditure.services.ts
@@ -3,23 +3,44 @@ import { IChurchService } from '../churchService'
 import { IBaseExpenditure, IExpenditureRes, IExpendituresRes } from './index'
 
 export const getExpenditures = async (
-  churchServiceId?: IChurchService['_id']
+  churchServiceId?: IChurchService['_id'],
+  cookie?: string
 ) => {
   if (churchServiceId) {
     const { data } = await makeRequest.get<IExpendituresRes>(
-      `/churchServices/${churchServiceId}/expenditures`
+      `/churchServices/${churchServiceId}/expenditures`,
+      {
+        withCredentials: true,
+        headers: {
+          Cookie: cookie,
+        },
+      }
     )
     return data
   } else {
-    const { data } = await makeRequest.get<IExpendituresRes>('/expenditures')
+    const { data } = await makeRequest.get<IExpendituresRes>('/expenditures', {
+      withCredentials: true,
+      headers: {
+        Cookie: cookie,
+      },
+    })
     return data
   }
 }
 
-export const addExpenditure = async (expenditure: IBaseExpenditure) => {
+export const addExpenditure = async (
+  expenditure: IBaseExpenditure,
+  cookie?: string
+) => {
   const { data } = await makeRequest.post<IExpenditureRes>(
     `/expenditures`,
-    expenditure
+    expenditure,
+    {
+      withCredentials: true,
+      headers: {
+        Cookie: cookie,
+      },
+    }
   )
   return data
 }
