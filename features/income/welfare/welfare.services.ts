f70a3cead4641763328d9b68238792a630fa9226
Dammy Ola <dammyolapaix@gmail.com>
2023-02-15 16:48:39 +0000

    Fetch welfares with cookie headers at getServerSideProps and cookie header to the endpoint

--- features/income/welfare/welfare.services.ts
+++ features/income/welfare/welfare.services.ts
@@ -1,12 +1,22 @@
 import { makeRequest } from '../../../lib'
 import { IBaseWelfare, IWelfareRes, IWelfaresRes } from './index'
 
-export const getWelfares = async () => {
-  const { data } = await makeRequest.get<IWelfaresRes>('/welfares')
+export const getWelfares = async (cookie?: string) => {
+  const { data } = await makeRequest.get<IWelfaresRes>('/welfares', {
+    withCredentials: true,
+    headers: {
+      Cookie: cookie,
+    },
+  })
   return data
 }
 
-export const addWelfare = async (welfare: IBaseWelfare) => {
-  const { data } = await makeRequest.post<IWelfareRes>('/welfares', welfare)
+export const addWelfare = async (welfare: IBaseWelfare, cookie?: string) => {
+  const { data } = await makeRequest.post<IWelfareRes>('/welfares', welfare, {
+    withCredentials: true,
+    headers: {
+      Cookie: cookie,
+    },
+  })
   return data
 }
