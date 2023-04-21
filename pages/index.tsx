13a6274d9e075e4bd706a32e64745fdfcd6b5897
Dammy ola <dammyolapaix@gmail.com>
2023-04-21 07:42:10 +0000

    Protected the home/dashboard page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import { Layout } from '../components'
+import cookie from 'cookie'
 
 const Home: NextPage = () => {
   return (
@@ -7,6 +8,38 @@ const Home: NextPage = () => {
       <h3 className="text-4xl font-bold">Dashboard</h3>
     </Layout>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const cookieHeaders = req.headers.cookie
+
+  if (!cookieHeaders) {
+    res.writeHead(302, { Location: '/login' })
+    res.end()
+    return {
+      props: {
+        success: false,
+        error: 'Access Denied',
+      },
+    }
+  } else {
+    const { token } = cookie.parse(cookieHeaders)
+
+    if (!token) {
+      res.writeHead(302, { Location: '/login' })
+      res.end()
+      return {
+        props: {
+          success: false,
+          error: 'Access Denied',
+        },
+      }
+    }
+  }
+
+  return {
+    props: {},
+  }
 }
 
 export default Home
